90ab87be8e2eae8ac4f88bb11357bfd9ab76c06d
wouter <wouter173@outlook.com>
2019-04-07 20:07:14 +0200

    debug update :athletic_shoe:

--- index.py
+++ index.py
@@ -3,13 +3,35 @@ import deco
 import timeFile as time
 
 loop = False
+
+debugTimes = (
+  "12:00am",
+  "12:01am",
+  "1:00am",
+  "11:00am",
+  "11:59am",
+  "12:00pm",
+  "12:01pm",
+  "1:00pm",
+  "11:00pm",
+  "11:59pm",
+)
+
+if os.uname()[0] == "Darwin":
+  clear = "clear"
+elif os.uname()[0] == "Linux":
+  clear = "clear"
+else:
+  clear = "cls"
   
-def main():
+def main(commands = True):
   os.system("clear")
   print(deco.center("-=[ time converter ]=-", "-"))
-  print(deco.green("type exit to exit the program"))
-  print("enter a time seperated by an ':' and end it with pm or am.")
-  print("example: " + deco.green("09:45am"))
+  if commands == True:
+    print(deco.green("type exit to exit the program"))
+    print(deco.green("type debug to debug some basic times"))
+    print("enter a time seperated by an ':' and end it with pm or am.")
+    print("example: " + deco.green("09:45am"))
 
 if __name__ == "__main__":
   main()
@@ -20,9 +42,21 @@ while loop == True:
   inp = input("> ")
   
   if inp == "exit":
-    os.system("clear")
+    os.system(clear)
     print("goodbye!")
     exit()
+
+  elif inp == "debug":
+    os.system(clear)
+    main(False)
+    for x in debugTimes:
+      space = ""
+      if len(x) == 6:
+        space = " "
+      print(x + space + " -> " + deco.green(time.parse(x)))
+    input("> ")
+    main()
+
 
   else: 
     print(deco.green(time.parse(inp)))
--- timeFile.py
+++ timeFile.py
@@ -1,31 +1,33 @@
-def error ():
-  print("You entered a time that does not exist")
-
 def parse (time):
+
+  time.replace(" ", "")
+
+  error = "You entered a time that does not exist"
+
   time = time.split(":")
   if len(time) != 2 or len(time[1]) != 4:
-    return error()
+    return error
     
 
   hour = time[0]
   minutes = "".join([time[1][0],time[1][1]])
   ampm = "".join([time[1][2],time[1][3]])
 
-  if len(hour) != 2 or len(minutes) != 2 or len(ampm) != 2:
-    return error()
+  if (len(hour) != 2 and len(hour) != 1) or len(minutes) != 2 or len(ampm) != 2:
+    return error
 
   try:
     hour = int(hour)
     minutes = int(minutes)
     ampm = str(ampm)
   except:
-    return error()
+    return error
 
   if type(hour) is not int or type(minutes) is not int or type(ampm) is not str:
-    return error()
+    return error
 
   if (hour > 12 or hour < 1) or (minutes > 59 or minutes < 0) or (ampm != 'am' and ampm != 'pm'):
-    return error()
+    return error
 
   if ampm == "pm":
     hour += 12
@@ -43,6 +45,9 @@ def parse (time):
   
   if(minutes == "0"):
     minutes = "00"
+  
+  if(len(minutes) == 1):
+    minutes = "0" + minutes
 
   return str(hour) + ":" + str(minutes)
 
